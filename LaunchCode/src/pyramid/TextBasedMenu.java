438b1790369cb0d02877d33e077c79a0da19780c
Marie Byleen <mbyleen@gmail.com>
2016-07-09 10:15:39 -0500

    Remove unused code from menu class
    remove the code used to specify menu options directly, which returns a
    menu number. It belongs in a different class, and this project does not
    require that functionality

--- LaunchCode/src/pyramid/TextBasedMenu.java
+++ LaunchCode/src/pyramid/TextBasedMenu.java
@@ -38,17 +38,4 @@ class TextBasedMenu {
 		System.out.print("\n");
 	}
 	
-	int getNumberOfUserSelectionFromMenu(String...options) {
-		printOptionsList(options);
-		int optionSelected = optionPrompter.getBoundedIntegerInput(1, options.length, prompt);													 
-		return optionSelected-1;
-	}
-	
-	private void printOptionsList(String[] options) {
-		System.out.println();
-		for (int i = 0; i < options.length; i++)
-			System.out.printf("   (%d) %s\n", i+1, options[i]);
-		System.out.println();
-	}
-	
 }
