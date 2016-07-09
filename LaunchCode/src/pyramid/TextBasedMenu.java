f2b0d5af6c0f285b37c531c2699fd1ab93505992
Marie Byleen <mbyleen@gmail.com>
2016-07-09 09:50:19 -0500

    Alter the text based menu to associate object strategies with menu items

--- LaunchCode/src/pyramid/TextBasedMenu.java
+++ LaunchCode/src/pyramid/TextBasedMenu.java
@@ -1,18 +1,48 @@
 package pyramid;
 
+import java.util.ArrayList;
+
 class TextBasedMenu {
 	
 	private IntegerInputPrompt optionPrompter;
 	private String prompt;
+	private ArrayList<String> optionTexts;
+	private ArrayList<Object> actions;
 	
 	TextBasedMenu() {
 		optionPrompter = new IntegerInputPrompt();
 		prompt = "Select an option number: ";
+		optionTexts = new ArrayList<String>();
+		actions = new ArrayList<Object>();
 	}
 	
-	int getUserSelectionFromMenu(String...options) {
+	void addOption(String optionText, Object action) {
+			optionTexts.add(optionText);
+			actions.add(action);
+	}
+	
+	Object getSelectionFromMenu() throws NullPointerException {
+		if (optionTexts.size() == 0) 
+			throw new NullPointerException();
+		printOptionsList();
+		int optionSelected = optionPrompter.getBoundedIntegerInput
+																			(1, optionTexts.size(), prompt);
+		Object actionSelected = actions.get(optionSelected-1);
+		return actionSelected;
+	}
+			
+	private void printOptionsList() {
+		System.out.print("\n");
+		for (int i = 0; i < optionTexts.size(); i++) {
+			System.out.printf("   (%d, args) %s\n\n", i+1, optionTexts.get(i));
+		}
+	}
+	
+	
+	int getNumberOfUserSelectionFromMenu(String...options) {
 		printOptionsList(options);
-		int optionSelected = optionPrompter.getBoundedIntegerInput(1, options.length, prompt);
+		int optionSelected = optionPrompter.getBoundedIntegerInput
+																							(1, options.length, prompt);
 		return optionSelected-1;
 	}
 	
