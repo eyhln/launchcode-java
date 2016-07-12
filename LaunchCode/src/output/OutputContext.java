0216d58dc8395d08dfc4fbd1db03c1a362fa7b00
Marie Byleen <mbyleen@gmail.com>
2016-07-12 15:59:41 -0500

    Move creation of objects from classes that implement the PyramidPrinter interface to this class

--- LaunchCode/src/output/OutputContext.java
+++ LaunchCode/src/output/OutputContext.java
@@ -6,8 +6,11 @@ public class OutputContext {
 	
 	private PyramidPrinter out;
 	
-	public OutputContext(PyramidPrinter out) {
-		this.out = out;
+	public OutputContext(String outputType) {
+		if (outputType.equals("standard output")) 
+			out = new PyramidToStandardOutputPrinter();
+		else if (outputType.equals("file")) 
+			out = new PyramidToFilePrinter();
 	}
 	
 	public void outputPyramid(Pyramid pyramidToOutput) {
