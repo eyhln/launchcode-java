8b93bc773c24b5de089dc581e9fb9a7acba3742d
Marie Byleen <mbyleen@gmail.com>
2016-07-12 15:55:21 -0500

    Change the action type from Object to String to avoid using an object constructor call as a parameter in code that uses the menu

--- LaunchCode/src/pyramid/TextBasedMenu.java
+++ LaunchCode/src/pyramid/TextBasedMenu.java
@@ -6,29 +6,29 @@ class TextBasedMenu {
 	
 	private IntegerInputPrompt optionPrompter;
 	private ArrayList<String> optionTexts;
-	private ArrayList<Object> actions;
+	private ArrayList<String> actions;
 	private String prompt;
 	
 	TextBasedMenu() {
 		optionPrompter = new IntegerInputPrompt();
 		optionTexts = new ArrayList<String>();
-		actions = new ArrayList<Object>();
+		actions = new ArrayList<String>();
 		prompt = "Select an option number: ";
 	}
 	
-	void addOption(String optionText, Object action) {
+	void addOption(String optionText, String action) {
 			optionTexts.add(optionText);
 			actions.add(action);
 	}
 	
-	Object getSelectionFromMenu() throws NullPointerException {
+	String getSelectionFromMenu() throws NullPointerException {
 		if (optionTexts.size() == 0) 
 			throw new NullPointerException();
 		printOptionsList();
 		optionPrompter.setNotAnIntegerMessage("Error: Not a menu item number");
 		int optionSelected = 
 				optionPrompter.getBoundedIntegerInput(1, optionTexts.size(), prompt);			 
-		Object actionSelected = actions.get(optionSelected-1);
+		String actionSelected = actions.get(optionSelected-1);
 		return actionSelected;
 	}
 			
