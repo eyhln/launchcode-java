96d5a44da2b5669e655bfc7426be0dcb7751709c
Marie Byleen <mbyleen@gmail.com>
2016-07-18 15:12:16 -0500

    Update to reflect changes in constructor of pyramid

--- LaunchCode/src/mario/pyramid/PyramidFactory.java
+++ LaunchCode/src/mario/pyramid/PyramidFactory.java
@@ -2,7 +2,7 @@ package mario.pyramid;
 
 public class PyramidFactory {
 	
-	public Pyramid getPyramid(int heightInSteps){
-		return new MarioPyramid(heightInSteps);
+	public Pyramid getPyramid(int heightInSteps, PyramidPrinter printer){
+		return new MarioPyramid(heightInSteps, printer);
 	}
 }	
