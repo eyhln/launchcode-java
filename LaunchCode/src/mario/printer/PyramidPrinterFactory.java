b1b070adf2a1248ff883dd3bc2ac32223adb0c24
Marie Byleen <mbyleen@gmail.com>
2016-07-22 09:51:35 -0500

    Change factories to use static methods

--- LaunchCode/src/mario/printer/PyramidPrinterFactory.java
+++ LaunchCode/src/mario/printer/PyramidPrinterFactory.java
@@ -1,14 +1,13 @@
 package mario.printer;
 
 public class PyramidPrinterFactory {
-
-	public PyramidPrinter getPyramidPrinter(int option){
-		if (!(option == 1 || option == 2)) 
-			throw new IllegalArgumentException();
-		if (option == 1) 
-			return new PyramidToStandardOutputPrinter();
-		else if (option == 2)
-			return new PyramidToFilePrinter();
-		return null;
+	
+	public static PyramidPrinter getPyramidToStandardOutputPrinter() {
+		return new PyramidToStandardOutputPrinter();
 	}
+	
+	public static PyramidPrinter getPyramidToFilePrinter() {
+		return new PyramidToFilePrinter();
+	}
+
 }
