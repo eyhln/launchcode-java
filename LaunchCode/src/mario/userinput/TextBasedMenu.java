400735d5ecafc8b7e8dd25b3491b52d4343e1607
Marie Byleen <mbyleen@gmail.com>
2016-07-22 09:56:29 -0500

    Revert menu to work with objects rather than strings

--- LaunchCode/src/mario/userinput/TextBasedMenu.java
+++ LaunchCode/src/mario/userinput/TextBasedMenu.java
@@ -6,29 +6,29 @@ class TextBasedMenu {
 	
 	private IntegerInputPrompt optionPrompter;
 	private ArrayList<String> optionTexts;
-	private ArrayList<String> actions;
+	private ArrayList<Object> actions;
 	private String prompt;
 	
 	TextBasedMenu() {
 		optionPrompter = new IntegerInputPrompt();
 		optionTexts = new ArrayList<String>();
-		actions = new ArrayList<String>();
+		actions = new ArrayList<Object>();
 		prompt = "Select an option number: ";
 	}
 	
-	void addOption(String optionText, String action) {
+	void addOption(String optionText, Object action) {
 			optionTexts.add(optionText);
 			actions.add(action);
 	}
 	
-	String getSelectionFromMenu() throws NullPointerException {
+	Object getSelectionFromMenu() throws NullPointerException {
 		if (optionTexts.size() == 0) 
 			throw new NullPointerException();
 		printOptionsList();
 		optionPrompter.setNotAnIntegerMessage("Error: Not a menu item number");
 		int optionSelected = 
 				optionPrompter.getBoundedIntegerInput(1, optionTexts.size(), prompt);			 
-		String actionSelected = actions.get(optionSelected-1);
+		Object actionSelected = actions.get(optionSelected-1);
 		return actionSelected;
 	}
 			
