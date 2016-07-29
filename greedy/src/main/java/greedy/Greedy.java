f848a14d0b12aab4a74145a5f219e39b329ea8a8
Marie Byleen <mbyleen@gmail.com>
2016-07-29 13:20:32 -0500

    Set up dependency injection of the algorithm into the main class

--- greedy/src/main/java/greedy/Greedy.java
+++ greedy/src/main/java/greedy/Greedy.java
@@ -1,18 +1,32 @@
 package greedy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * User: mpmenne
  * Date: 6/18/14
  * Time: 3:05 AM
  */
 public class Greedy {
+	
+	 public static void main(String[] varArgs) {
+		 ApplicationContext context = 
+				 new ClassPathXmlApplicationContext("application-context.xml");
 
+		 Greedy greedy = (Greedy)context.getBean("greedy");
+		 greedy.start();
+	    }
+	
+	CoinCalculator coinCalculator;
 
-    public static void main(String[] varArgs) {
-        if (varArgs.length > 1) {
-            CoinCalculator coinCalculator = new CoinCalculator();
-            coinCalculator.calculateChange(varArgs[0]);
-        }
-        throw new IllegalArgumentException("No value given");
-    }
+	public Greedy(CoinCalculator coinCalculator) {
+		this.coinCalculator = coinCalculator;
+	}
+	
+	public void start() {
+		
+	}
+   
 
 }
