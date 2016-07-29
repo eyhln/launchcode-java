3f3c2830d6dd27e696ddc53efc60eaf6f6c3e27a
Marie Byleen <mbyleen@gmail.com>
2016-07-29 12:56:49 -0500

    Implement basic coin-minimizing functionality
    Calculate minimum number of coins needed to make change using a
    hard-coded array of values for USD coins

--- greedy/src/main/java/greedy/CoinCalculator.java
+++ greedy/src/main/java/greedy/CoinCalculator.java
@@ -1,20 +1,33 @@
 package greedy;
-/**
- * User: mpmenne
- * Date: 6/18/14
- * Time: 3:06 AM
- */
+
+import java.util.Arrays;
+
 public class CoinCalculator {
-
-
-    public String calculateChange(String amountOfChange) {
-        Integer change = Integer.parseInt(amountOfChange.substring(amountOfChange.length() - 2));
-        if(change > 9) {
-            return "1 dime";
-        } else if (change > 0) {
-            return "1 penny";
-        } else {
-            return "No coins returned";
-        }
+	
+	private int totalRemaining;
+	int[] coinValues = {1,5,10,25,100}; 
+    
+    public int calculateChange(int nonNegativeAmountInCents) {
+    	totalRemaining = nonNegativeAmountInCents;
+    	processCoinValues();
+    	int currCoinIndex = 0;
+    	int minNumOfCoinsNeeded = 0;
+		while (totalRemaining > 0) {
+			int remainder = totalRemaining % coinValues[currCoinIndex];
+			minNumOfCoinsNeeded += (totalRemaining - remainder) / coinValues[currCoinIndex];
+			totalRemaining = remainder;
+			currCoinIndex++;
+		}
+		return minNumOfCoinsNeeded;
     }
+    
+    void processCoinValues() {
+    	Arrays.sort(coinValues);
+    	int[] reverse = new int[coinValues.length];
+    	for (int i = 0; i < reverse.length; i++) {
+    		reverse[i] = coinValues[coinValues.length-1-i];
+    	}
+    	coinValues = reverse;
+    }
+
 }
--- greedy/src/test/java/greedy/CoinCalculatorTest.java
+++ greedy/src/test/java/greedy/CoinCalculatorTest.java
@@ -4,38 +4,61 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * User: mpmenne
- * Date: 6/18/14
- * Time: 3:28 AM
- */
+import org.junit.Before;
+
 public class CoinCalculatorTest {
+	
+	CoinCalculator cc;
 
-    @Test
-    public void zeroChangeMeansYouGetZeroCoins() {
-        CoinCalculator coinCalculator = new CoinCalculator();
-
-        String coinMessage = coinCalculator.calculateChange("$3.00");
-
-        assertEquals("No coins returned", coinMessage);
-    }
-
-    @Test
-    public void oneCentShouldGiveYouOnePenny() {
-        CoinCalculator coinCalculator = new CoinCalculator();
-
-        String coinMessage = coinCalculator.calculateChange("$0.01");
-
-        assertEquals("1 penny", coinMessage);
-    }
-
-    @Test
-    public void tenCentsShouldGiveYouOneDime() {
-        CoinCalculator coinCalculator = new CoinCalculator();
-
-        String coinMessage = coinCalculator.calculateChange("$0.10");
-
-        assertEquals("1 dime", coinMessage);
-    }
+	@Before
+	public void initialize() {
+		cc = new CoinCalculator();
+	}
+	
+	@Test
+	public void testProcessCoinValuesEmptyArray() {
+		int[] empty = new int[0];
+		cc.coinValues = empty;
+		cc.processCoinValues();
+		for (int i = 0; i < empty.length; i++) {
+			assertEquals(empty[i], cc.coinValues[i]);
+		}
+	}
+	
+	@Test
+	public void testProcessCoinValuesUnorderedArray() {
+		int[] unordered = {1,3,90,43,18,0};
+		int[] sorted = {90,43,18,3,1,0};
+		cc.coinValues = unordered;
+		cc.processCoinValues();
+		for (int i = 0; i < unordered.length; i++) {
+			assertEquals(sorted[i], cc.coinValues[i]);
+		}
+	}
+	
+	@Test
+	public void testCalculateChangeZero() {
+		assertEquals(0,cc.calculateChange(0));
+	}
+	
+	@Test
+	public void testCalculateChangeFive() {
+		assertEquals(1,cc.calculateChange(5));
+	}
+	
+	@Test
+	public void testCalculateChangeSix() {
+		assertEquals(2,cc.calculateChange(6));
+	}
+	
+	@Test
+	public void testCalculateChangeThirtyTwo() {
+		assertEquals(4,cc.calculateChange(32));
+	}
+	
+	@Test
+	public void testCalculateChangeFourHundredEleven() {
+		assertEquals(402,cc.calculateChange(40011));
+	}
 
 }
