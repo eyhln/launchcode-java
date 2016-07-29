0cbb7fb5a637cccad7007034d1f43a272bc1aa29
Marie Byleen <mbyleen@gmail.com>
2016-07-29 09:33:56 -0500

    initial commit - create maven and spring enabled project

---
+++ greedy/src/test/java/greedy/GreedyApplicationTests.java
@@ -0,0 +1,16 @@
+package greedy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GreedyApplication.class)
+public class GreedyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
