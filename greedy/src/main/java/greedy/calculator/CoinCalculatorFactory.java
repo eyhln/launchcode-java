4df2fef5e7146ece075ebd56276de69dd861e08f
Marie Byleen <mbyleen@gmail.com>
2016-08-02 14:44:29 -0500

    Create Euro coin specification and set up factory to use it

--- greedy/src/main/java/greedy/calculator/CoinCalculatorFactory.java
+++ greedy/src/main/java/greedy/calculator/CoinCalculatorFactory.java
@@ -4,19 +4,26 @@ import java.util.Currency;
 import java.util.Locale;
 
 import greedy.coins.CoinSpecification;
+import greedy.coins.EuroCoinSpecification;
 import greedy.coins.USDollarCoinSpecification;
 
 public class CoinCalculatorFactory {
 	
 	Currency USDollar;
+	Currency Euro;
 	
 	public CoinCalculatorFactory() {
 		USDollar = Currency.getInstance(Locale.US);
+		Euro = Currency.getInstance(Locale.GERMANY);
 	}
 	
 	public CoinCalculator getCoinSpecification(Currency currency) {
 		if (currency.equals(USDollar)) {
 			CoinSpecification coinSpec = new USDollarCoinSpecification();
+			return new CoinCalculatorImpl(coinSpec.getCoinMap());
+		}
+		if (currency.equals(Euro)) {
+			CoinSpecification coinSpec = new EuroCoinSpecification();
 			return new CoinCalculatorImpl(coinSpec.getCoinMap());
 		}
 		return null;
