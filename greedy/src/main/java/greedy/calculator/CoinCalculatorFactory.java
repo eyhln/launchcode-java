19afa951516eb9aab25772e962ae709022e74cf5
Marie Byleen <mbyleen@gmail.com>
2016-08-03 15:48:45 -0500

    use two methods to extract data from coin specification
    was using a hashmap, changed to two arrays

--- greedy/src/main/java/greedy/calculator/CoinCalculatorFactory.java
+++ greedy/src/main/java/greedy/calculator/CoinCalculatorFactory.java
@@ -3,7 +3,6 @@ package greedy.calculator;
 import java.util.Currency;
 import java.util.Locale;
 
-import greedy.coins.CoinSpecification;
 import greedy.coins.EuroCoinSpecification;
 import greedy.coins.USDollarCoinSpecification;
 
@@ -19,12 +18,12 @@ public class CoinCalculatorFactory {
 	
 	public CoinCalculator getCoinSpecification(Currency currency) {
 		if (currency.equals(USDollar)) {
-			CoinSpecification coinSpec = new USDollarCoinSpecification();
-			return new CoinCalculatorImpl(coinSpec.getCoinMap());
+			USDollarCoinSpecification coinSpec = new USDollarCoinSpecification();
+			return new CoinCalculator(coinSpec.getCoinValues(), coinSpec.getCoinNameCodes());
 		}
 		if (currency.equals(Euro)) {
-			CoinSpecification coinSpec = new EuroCoinSpecification();
-			return new CoinCalculatorImpl(coinSpec.getCoinMap());
+			EuroCoinSpecification coinSpec = new EuroCoinSpecification();
+			return new CoinCalculator(coinSpec.getCoinValues(), coinSpec.getCoinNameCodes());
 		}
 		return null;
 	}
--- greedy/src/test/java/greedy/calculator/EuroCoinCalculatorTest.java
+++ greedy/src/test/java/greedy/calculator/EuroCoinCalculatorTest.java
@@ -1,65 +1,37 @@
 package greedy.calculator;
 
 import org.junit.Test;
-
-import greedy.calculator.CoinCalculatorImpl;
-
 import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 
 import org.junit.Before;
 
-public class CoinCalculatorImplTest {
+public class EuroCoinCalculatorTest {
 	
-	CoinCalculatorImpl cc;
-	HashMap<Integer,String> coinSpecification;
+	CoinCalculator cc;
 	HashMap<String,Integer> coinsNeeded;
 
 
 	@Before
 	public void initialize() {
-		cc = new CoinCalculatorImpl(coinSpecification);
+		cc = new CoinCalculator(null, null);
 		coinsNeeded = new HashMap<String,Integer>();
 	}
 	
 	@Test
-	public void testProcessCoinValuesEmptyArray() {
-		Object[] empty = new Object[0];
-		cc.coinValues = empty;
-		cc.processCoinValues();
-		for (int i = 0; i < empty.length; i++) {
-			assertEquals(empty[i], cc.coinValues[i]);
-		}
-	}
-	
-	@Test
-	public void testProcessCoinValuesUnorderedArray() {
-		Object[] unordered = {1,3,90,43,18,0};
-		int[] sorted = {90,43,18,3,1,0};
-		cc.coinValues = unordered;
-		cc.processCoinValues();
-		for (int i = 0; i < unordered.length; i++) {
-			assertEquals(sorted[i], cc.coinValues[i]);
-		}
-	}
-	
-	@Test
 	public void testCalculateChangeZero() {
-		setUSCoinSpec();
 		assertEquals(coinsNeeded, cc.calculateChange(0));
 	}
 	
 	@Test
 	public void testCalculateChangeFive() {
-		setUSCoinSpec();
 		coinsNeeded.put("coin.4", 1);
 		assertEquals(coinsNeeded, cc.calculateChange(5));
 	}
 	
 	@Test
 	public void testCalculateChangeSix() {
-		setUSCoinSpec();
 		coinsNeeded.put("coin.4", 1);
 		coinsNeeded.put("coin.5", 1);
 		assertEquals(coinsNeeded, cc.calculateChange(6));
@@ -67,7 +39,6 @@ public class CoinCalculatorImplTest {
 	
 	@Test
 	public void testCalculateChangeThirtyTwo() {
-		setUSCoinSpec();
 		coinsNeeded.put("coin.4", 1);
 		coinsNeeded.put("coin.2", 1);
 		coinsNeeded.put("coin.5", 2);
@@ -76,21 +47,11 @@ public class CoinCalculatorImplTest {
 	
 	@Test
 	public void testCalculateChangeFourHundredEleven() {
-		setUSCoinSpec();
 		coinsNeeded.put("coin.1",400);
 		coinsNeeded.put("coin.3", 1);
 		coinsNeeded.put("coin.5", 1);
 		assertEquals(coinsNeeded, cc.calculateChange(40011));
 	}
 	
-	private void setUSCoinSpec() {
-		coinSpecification = new HashMap<Integer,String>();
-		coinSpecification.put(100, "coin.1");
-		coinSpecification.put(25, "coin.2");
-		coinSpecification.put(10, "coin.3");
-		coinSpecification.put(5, "coin.4");
-		coinSpecification.put(1, "coin.5");
-		cc = new CoinCalculatorImpl(coinSpecification);
-	}
 
 }
