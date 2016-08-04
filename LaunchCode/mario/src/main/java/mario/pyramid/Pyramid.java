87be903c5f528086ddfdaffe844abc7a72c6eb57
Marie Byleen <mbyleen@gmail.com>
2016-08-04 14:17:02 -0500

    Make pyramids immutable
    remove the parameter from the buildPyramid method in class Pyramid and
    rely on the value being set in the constructor

--- LaunchCode/mario/src/main/java/mario/pyramid/Pyramid.java
+++ LaunchCode/mario/src/main/java/mario/pyramid/Pyramid.java
@@ -1,8 +1,49 @@
 package mario.pyramid;
 
-public interface Pyramid {
-
-	@Override
-	public String toString();
+public class Pyramid {
 	
+	private StringBuilder builder;
+	private int heightInSteps;
+	
+	Pyramid(int heightInSteps) {
+		builder = new StringBuilder();
+		this.heightInSteps = heightInSteps;
+	}
+	
+	@Override
+	public String toString() {
+		return buildPyramid();
+	}
+	
+	String buildPyramid() {
+		clearCharactersFromBuilder();
+		for (int lineNumber = 0; lineNumber < heightInSteps; lineNumber++) {
+			createLine(lineNumber);
+		}
+		return builder.toString();
+	}
+	
+	private void clearCharactersFromBuilder() {
+		builder.setLength(0);
+	}
+	
+	private void createLine(int lineNumber) {
+		startNewLine();
+		addSpacesToLine(lineNumber);
+		addHashesToLine(lineNumber);
+	}
+	
+	private void startNewLine() {
+		builder.append("\n");
+	}
+	 
+	private void addSpacesToLine(int lineNumber) {
+		for (int i = heightInSteps-2-lineNumber; i >= 0; i--)
+			builder.append(" ");
+	}
+	
+	private void addHashesToLine(int lineNumber) {
+		for (int i = 1+lineNumber; i >= 0; i--)
+			builder.append("#");
+	}
 }
