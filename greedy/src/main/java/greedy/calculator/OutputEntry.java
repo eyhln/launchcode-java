170838539d7dc06ff5fbba869e327f1081166dbc
Marie Byleen <mbyleen@gmail.com>
2016-08-05 14:01:25 -0500

    Create a thin wrapper for lines of output in order to enforce type

---
+++ greedy/src/main/java/greedy/calculator/OutputEntry.java
@@ -0,0 +1,21 @@
+package greedy.calculator;
+
+public class OutputEntry {
+	
+	String coinType;
+	int numberOfCoins;
+	
+	public OutputEntry(String coinType, int numberOfCoins) {
+		this.coinType = coinType;
+		this.numberOfCoins = numberOfCoins;
+	}
+	
+	public String getCoinType() {
+		return coinType;
+	}
+	
+	public int getNumberOfCoins() {
+		return numberOfCoins;
+	}
+
+}
