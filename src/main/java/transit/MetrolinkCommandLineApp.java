a671b4b6821f54e4a8f2314ac8360f01f91a4666
Marie Byleen <mbyleen@gmail.com>
2016-08-18 16:13:05 -0500

    Refactor
    set up variables to use when querying the time of the next train

--- src/main/java/transit/MetrolinkCommandLineApp.java
+++ src/main/java/transit/MetrolinkCommandLineApp.java
@@ -1,5 +1,6 @@
 package transit;
 
+import java.time.*;
 import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,14 +18,36 @@ public class MetrolinkCommandLineApp {
     }
 
     private void run() {
-        List<Stop> stopsAllStops = metrolinkDao.getStopsAllStops();
-        for (Stop stop : stopsAllStops) {
-            appOutput.print(String.format("--- %s ---", stop.getStopName()));
-        }
+    	List<Stop> stopsAllStops = getListOfAllStops();
+      print(stopsAllStops);  
+        appOutput.print("\nEnter the name of your current Metrolink Station location: ");
+        String location = appInput.getUserMetrolinkLocation();
+        Stop currentUserLocationStop;
+        for (Stop stop: stopsAllStops) {
+        	if (location.equals(stop.getStopName()))
+        		currentUserLocationStop = stop;
+        } 
+      LocalDateTime currentDateTime = LocalDateTime.now(); 
+      LocalTime currentTime = currentDateTime.toLocalTime();
+      //LocalTime timeOfNextTrain = metrolinkDao.getTimeOfNextTrain(stop, currentDayTime);
+        
+    }
+    
+    List<Stop> getListOfAllStops() {
+    	List<Stop> stopsAllStops = metrolinkDao.getStopsAllStops();
+    	return stopsAllStops;
+    }
+    
+    void print(List<Stop> stopsAllStops) {
+    	for (Stop stop : stopsAllStops) {
+        appOutput.print(String.format("--- %s ---", stop.getStopName()));
+    	}
     }
 
     private MetrolinkDao metrolinkDao;
     private AppOutput appOutput;
+    private AppInput appInput;
+    
 
     public void setMetrolinkDao(MetrolinkDao metrolinkDao) {
         this.metrolinkDao = metrolinkDao;
@@ -32,5 +55,9 @@ public class MetrolinkCommandLineApp {
 
     public void setAppOutput(AppOutput appOutput) {
         this.appOutput = appOutput;
+    }
+    
+    public void setAppInput(AppInput appInput) {
+    	this.appInput = appInput;
     }
 }
