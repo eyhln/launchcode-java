727cb2ed601028d1097e9428ab7767219922e136
Marie Byleen <mbyleen@gmail.com>
2016-08-18 16:11:43 -0500

    Add method to retrieve time of next train

--- src/main/java/transit/MetrolinkDao.java
+++ src/main/java/transit/MetrolinkDao.java
@@ -1,9 +1,12 @@
 package transit;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface MetrolinkDao {
 
   public List<Stop> getStopsAllStops();
+  public LocalTime getTimeOfNextTrain(Stop stop, LocalDateTime currentDayTime);
 
 }
