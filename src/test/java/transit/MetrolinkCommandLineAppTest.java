48fcc1dd74fca9ae4453e6db562b659a9c339e48
Marie Byleen <mbyleen@gmail.com>
2016-08-23 15:57:51 -0500

    Use mock objects to test
    Integrate use of the mockito framework and add tests for failure
    conditions

--- src/test/java/transit/MetrolinkCommandLineAppTest.java
+++ src/test/java/transit/MetrolinkCommandLineAppTest.java
@@ -1,6 +1,7 @@
 package transit;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.*;
 import java.time.*;
@@ -8,88 +9,102 @@ import java.time.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class MetrolinkCommandLineAppTest {
 	
 	private MetrolinkCommandLineApp app;
 	private AppOutput testAppOutput;
-	private AppInput testAppInput;
-	private MetrolinkDao testDao;
+	private AppInput mockAppInput;
+	private MetrolinkDao mockDao;
 	
 	@Before
 	public void initialize() {
 		app = new MetrolinkCommandLineApp(); 
-		testAppOutput = new TestAppOutput();
+		createDependencies();
+		associateDependencies();
+	}
+	
+	private void createDependencies() {
+		testAppOutput = new TestAppOutput();		
+		mockAppInput = mock(AppInput.class);
+		mockDao = mock(MetrolinkDao.class);
+	}
+	
+	private void associateDependencies() {
 		app.setAppOutput(testAppOutput);
-		testAppInput = new TestAppInput();
-		app.setAppInput(testAppInput);
-		testDao = new TestDao();
-		app.setMetrolinkDao(testDao);
+		app.setAppInput(mockAppInput);
+		app.setMetrolinkDao(mockDao);
 	}
 	
 	@Test
 	public void testPrintListOfStops() {
-		app.printListOfStops();
-		assertEquals("--- TEST ---", ((TestAppOutput) testAppOutput).getOutput());
+		List<Stop> testStopList = buildListOfStops();
+		when(mockDao.getStopsAllStops()).thenReturn(testStopList);
+		app.printAndReturnListOfStops();
+		assertEquals("--- TEST ---", ((TestAppOutput) testAppOutput).getString());
+	}
+	
+	private List<Stop> buildListOfStops() {
+		Stop testStop1 = new Stop();
+		testStop1.setStopName("TEST");
+		List<Stop> stops = new ArrayList<Stop>();
+		stops.add(testStop1);
+		return stops;
 	}
 	
 	@Test
 	public void testGetUserLocation() {
-		Stop testStop = new Stop();
-		testStop.setStopName("TEST");
+		when(mockAppInput.getUserMetrolinkLocation()).thenReturn("Test");
+		List<Stop> testStopList = buildListOfStops();
 		
-		app.printListOfStops();
-		Stop stop = app.getUserLocation();
+		Stop stop = app.getUserLocation(testStopList);
 		
-		assertEquals(testStop.getStopName(), stop.getStopName());
+		assertEquals("TEST", stop.getStopName());
+	}
+	
+	@Test 
+	public void testGetUserLocationInvalidName() {
+		when(mockAppInput.getUserMetrolinkLocation()).thenReturn("Invalid");
+		List<Stop> testStopList = buildListOfStops();
+		
+		app.getUserLocation(testStopList);
+		
+		assertEquals("Error: not a valid stop name", ((TestAppOutput) testAppOutput).getString());
 	}
 	
 	@Test
 	public void testPrintArrivalTimeOfNextTrain() {
-		app.printArrivalTimeOfNextTrain();
+		LocalDateTime testDateTime = LocalDateTime.of(2016,01,01,04,00,00);
+		LocalTime testTime = testDateTime.toLocalTime();
+		Duration tenMinutes = Duration.ofMinutes(10);
+		LocalTime testTrainArrivalTime = testTime.plus(tenMinutes);
+		when(mockDao.getTimeOfNextTrain(null, testDateTime)).thenReturn(testTrainArrivalTime);
+		
+		app.printWaitTimeToNextTrain(testDateTime);
 		
 		assertEquals("The next train is arriving in 10 minutes.", 
-				((TestAppOutput) testAppOutput).getOutput());
+				((TestAppOutput) testAppOutput).getString());
+	}
+	
+	@Test
+	public void testPrintArrivalTimeOfNextTrainNoTimesFound() {
+		when(mockDao.getTimeOfNextTrain(null, null)).thenReturn(null);
+		
+		app.printWaitTimeToNextTrain(null);
+		
+		assertEquals("No train times found for today.", ((TestAppOutput) testAppOutput).getString());
 	}
 	
 	private class TestAppOutput implements AppOutput {
-		private String output;
+		String string;
 		@Override
-		public void print(String format) {
-			output = format;
+		public String print(String format) {
+			string = format;
+			return format;
 		}
-		public String getOutput() {
-			return output;
-		}
-	}
-	
-	private class TestAppInput implements AppInput {
-		@Override
-		public String getUserMetrolinkLocation() {
-			return "Test";
-		}
-	}
-	
-	private class TestDao implements MetrolinkDao {
-
-		@Override
-		public List<Stop> getStopsAllStops() {
-			Stop testStop1 = new Stop();
-			testStop1.setStopName("TEST");
-			List<Stop> stops = new ArrayList<Stop>();
-			stops.add(testStop1);
-			return stops;
-		}
-
-		@Override
-		public LocalTime getTimeOfNextTrain(Stop stop, LocalDateTime currentDayTime) {
-			Duration tenMinutes = Duration.ofMinutes(10);
-			LocalTime testTime = LocalTime.now().plus(tenMinutes);
-			return testTime;
+		public String getString() {
+			return string;
 		}
 		
 	}
-	
-	
 
 }
