11f16f02b07cb943df3452a0e84efc5c494ae7f9
Marie Byleen <mbyleen@gmail.com>
2016-08-23 16:03:32 -0500

    Refactor naming for consistency

--- src/test/java/transit/dao/SqliteJDBCDaoTest.java
+++ src/test/java/transit/dao/SqliteJDBCDaoTest.java
@@ -10,13 +10,11 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import transit.AppOutput;
 import transit.Stop;
 
 public class SqliteJDBCDaoTest {
 	
 	SqliteJDBCDao dao;
-	AppOutput testAppOutput;
 	Stop testStop;
 	LocalDateTime testDateTime;
 	
@@ -24,7 +22,7 @@ public class SqliteJDBCDaoTest {
 	public void initialize() {
 		dao = new SqliteJDBCDao();
 		testStop = new Stop();
-		testStop.setStopName("UNION STA METROLINK STATION");
+		testStop.setStopName("UNION STA");
 		testDateTime = LocalDateTime.of(2016,01,01,04,00,00);
 	}
 
@@ -41,7 +39,7 @@ public class SqliteJDBCDaoTest {
 	}
 	
 	@Test
-	public void testHolidayScheduleChange() {
+	public void testHolidayScheduleChangeTrainTime() {
 		Stop unionSta = new Stop();
 		unionSta.setStopName("UNION STA");
 		LocalDateTime fourthOfJuly = LocalDateTime.of(2016,7,4,16,00,00);
@@ -53,14 +51,14 @@ public class SqliteJDBCDaoTest {
 	}
 	
 	@Test
-	public void testGetTimeOfNextTrain() {
+	public void testWeekdayAfternoonTrainTime() {
 		LocalTime expected = LocalTime.of(04,26,00);
 		LocalTime nextTrain = dao.getTimeOfNextTrain(testStop, testDateTime);
 		assertEquals(expected, nextTrain);
 	}
 	
 	@Test
-	public void testGetSaturdayTrainTime() {
+	public void testSaturdayTrainTime() {
 		LocalTime expected = LocalTime.of(04, 22);
 		LocalTime nextTrain = dao.getTimeOfNextTrain(testStop, testDateTime);
 		assertEquals(expected, nextTrain);
@@ -69,15 +67,16 @@ public class SqliteJDBCDaoTest {
 	@Test
 	public void testLateNightTrainTime() {
 		LocalTime expected = LocalTime.of(01,12);
-		LocalDateTime lateDateTime = LocalDateTime.of(2016,8,01,01,00,00);
+		LocalDateTime lateDateTime = LocalDateTime.of(2016,8,01,01,00,12,345);
 		LocalTime nextTrain = dao.getTimeOfNextTrain(testStop, lateDateTime);
 		assertEquals(expected, nextTrain);
 	}
 	
-	@Test @Ignore //(expected = RuntimeException.class)
+	@Test @Ignore("") //(expected = RuntimeException.class)
 	public void testThrowsException() {
-		Stop namelessStop = new Stop();
-		dao.getTimeOfNextTrain(namelessStop, testDateTime);
+		Stop notInDatabaseStop = new Stop();
+		notInDatabaseStop.setStopName("Test");
+		dao.getTimeOfNextTrain(notInDatabaseStop, testDateTime);
 	}
 
 }
