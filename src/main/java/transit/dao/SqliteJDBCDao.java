0765b97387c3dd3eb23f7d16de6d214a2d3ba373
Marie Byleen <mbyleen@gmail.com>
2016-08-25 14:11:27 -0500

    Refactor for clarity
    split larger methods into several smaller in order to maintain a single
    level of abstraction in each method

--- src/main/java/transit/dao/SqliteJDBCDao.java
+++ src/main/java/transit/dao/SqliteJDBCDao.java
@@ -19,10 +19,7 @@ public class SqliteJDBCDao implements MetrolinkDao {
   	
     public List<Stop> getStopsAllStops() {
         try (Connection connection = getConnection();) {
-        	PreparedStatement preparedStatement = 
-          		connection.prepareStatement(
-          				"SELECT DISTINCT stop_name FROM stops " +
-          				"WHERE stop_name LIKE '%METROLINK%STATION';");
+        	PreparedStatement preparedStatement = prepareGetAllStopsStatement(connection);
           ResultSet resultSet = preparedStatement.executeQuery();
           List<Stop> stops = createListOfStops(resultSet);
           return stops;
@@ -30,6 +27,13 @@ public class SqliteJDBCDao implements MetrolinkDao {
             throw new RuntimeException("Error retrieving stops");
         }
     }
+    		
+    		private PreparedStatement prepareGetAllStopsStatement(Connection connection) throws SQLException {
+    			PreparedStatement preparedStatement = connection.prepareStatement(
+          				"SELECT DISTINCT stop_name FROM stops " +
+          				"WHERE stop_name LIKE '%METROLINK%STATION';");
+    			return preparedStatement;
+    		}
     
 		    private List<Stop> createListOfStops(ResultSet resultSet) throws SQLException {
 		    	List<Stop> stops = new ArrayList<Stop>();
@@ -49,39 +53,49 @@ public class SqliteJDBCDao implements MetrolinkDao {
 		    }
 		    
     public LocalTime getTimeOfNextTrain(Stop stop, LocalDateTime dateTime) {
-    	String stopName = stop.getStopName();
-    	String time = formatter.formatTimeToMatchDatabase(dateTime);
-    	String date = formatter.formatDateToMatchDatabase(dateTime);
-    	String dayName = dateTime.getDayOfWeek().toString();
-      try (Connection connection = getConnection();) {
-      	String statement = 
-    				"SELECT min(arrival_time) AS next_train \n" +
-      			"FROM stops \n" +
-    				"INNER JOIN stop_times ON stops.stop_id = stop_times.stop_id \n" +
-    				"INNER JOIN trips ON stop_times.trip_id = trips.trip_id \n" +
-    				"INNER JOIN calendar ON trips.service_id = calendar.service_id \n" +
-    				"INNER JOIN calendar_dates ON calendar.service_id = calendar_dates.service_id \n" +
-    				"WHERE stop_name LIKE '" + stopName + " METROLINK%STATION' \n" +
-    				"AND ((date = " + date + " AND exception_type = 1) \n" +
-    				"OR ((date != " + date + ") AND " + dayName + ")) \n" +
-    				"AND (arrival_time > '" + time + "' \n" +
-    				"OR ('" + time + "' >= (" +
-    				    "\tSELECT max(arrival_time) \n" + 
-    				    "\tFROM stops \n" + 
-    				    "\tINNER JOIN stop_times ON stops.stop_id = stop_times.stop_id \n" +
-    				    "\tWHERE stop_name LIKE '" + stopName + " METROLINK%STATION') \n" +
-    				    "\tAND arrival_time > '00:00:00'));\n"; 
+      try (Connection connection = getConnection();) {	
+  			String statement = constructQuery(stop, dateTime);
       	PreparedStatement preparedStatement = connection.prepareStatement(statement);
         ResultSet resultSet = preparedStatement.executeQuery();
-       	String nextTrainTimeString = resultSet.getString("next_train");
-        LocalTime nextTrainArrivalTime = 
-        		formatter.formatReturnStringToLocalTime(nextTrainTimeString);
-        return nextTrainArrivalTime;
+        return retrieveArrivalTime(resultSet);
       } catch (SQLException e) {
           throw new RuntimeException("Error retrieving next train time");
     	}
     }
     
+    		private String constructQuery(Stop stop, LocalDateTime dateTime) {
+        	String stopName = stop.getStopName();
+        	String time = formatter.formatTimeToMatchDatabase(dateTime);
+        	String date = formatter.formatDateToMatchDatabase(dateTime);
+        	String dayName = dateTime.getDayOfWeek().toString();
+          String statement = 
+        				"SELECT min(arrival_time) AS next_train \n" +
+          			"FROM stops \n" +
+        				"INNER JOIN stop_times ON stops.stop_id = stop_times.stop_id \n" +
+        				"INNER JOIN trips ON stop_times.trip_id = trips.trip_id \n" +
+        				"INNER JOIN calendar ON trips.service_id = calendar.service_id \n" +
+        				"INNER JOIN calendar_dates ON calendar.service_id = calendar_dates.service_id \n" +
+        				"WHERE stop_name LIKE '" + stopName + " METROLINK%STATION' \n" +
+        				"AND ((date = " + date + " AND exception_type = 1) \n" +
+        				"OR ((date != " + date + ") AND " + dayName + ")) \n" +
+        				"AND (arrival_time > '" + time + "' \n" +
+        				"OR ('" + time + "' >= (" +
+        				    "\tSELECT max(arrival_time) \n" + 
+        				    "\tFROM stops \n" + 
+        				    "\tINNER JOIN stop_times ON stops.stop_id = stop_times.stop_id \n" +
+        				    "\tWHERE stop_name LIKE '" + stopName + " METROLINK%STATION') \n" +
+        				    "\tAND arrival_time > '00:00:00'));\n"; 
+    			return statement;
+    		}
+    		
+    		private LocalTime retrieveArrivalTime(ResultSet resultSet) throws SQLException {
+    			String nextTrainTimeString = resultSet.getString("next_train");
+          LocalTime nextTrainArrivalTime = 
+          		formatter.formatReturnStringToLocalTime(nextTrainTimeString);
+          return nextTrainArrivalTime;
+    		}
+    
+    		
     static Connection getConnection() throws SQLException {
         try {
             Class.forName(ORG_SQLITE_JDBC);
